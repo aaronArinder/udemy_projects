ca98db42ce95e5d8ea757866715459682a4385dd
Aaron Arinder <aaronarinder@gmail.com>
2019-04-10 12:48:57 -0400

    udemy_projects init   - one place to house all the rando udemy stuff I don't want to lose   - initiliazed with electron video project from Grider

---
+++ electron_video/index.js
@@ -0,0 +1,16 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('videoSubmitted', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    mainWindow.webContents.send('videoMetadata', metadata.format.duration);
+  });
+});
