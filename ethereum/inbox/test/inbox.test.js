ff598782d79a3e0585ac3c80b0d28b01ff60b05e
Aaron Arinder <aaronarinder@gmail.com>
2019-05-17 14:34:51 -0400

    ethereum: basic tests added

--- ethereum/inbox/test/inbox.test.js
+++ ethereum/inbox/test/inbox.test.js
@@ -2,17 +2,21 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-beforeEach(() => {
+let accounts, inbox;
+beforeEach(async () => {
   // get list of accounts
-  web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-      console.log('fetchedAccounts', fetchedAccounts);
-    });
+  accounts = await web3.eth.getAccounts();
   // use an account to deploy contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('inbox', () => {
-  it('deploys a contract', () => {});
+  it('deploys a contract', () => {
+    console.log('inbox', inbox)
+  });
 });
 
