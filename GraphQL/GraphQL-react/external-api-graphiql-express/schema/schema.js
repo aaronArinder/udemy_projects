1a86c93c270a38bc2dfdbf91b83259a20e01d738
Aaron Arinder <aaronarinder@gmail.com>
2019-05-23 13:50:38 -0400

    GraphQL: relating schemas together

--- GraphQL/GraphQL-react/external-api-graphiql-express/schema/schema.js
+++ GraphQL/GraphQL-react/external-api-graphiql-express/schema/schema.js
@@ -8,14 +8,30 @@ const {
   GraphQLObjectType,
 } = graphql;
 
+// CompanyType schema
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
 
 // UserType schema
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
-    firstName: {type: GraphQLString },
+    firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve (parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(({ data }) => data);
+      }
+    }
   }
 });
 
